b47d837480c7e34a6dbe701cd447b5a3385201b4
Aaron Leitner <aaron.leitner14@gmail.com>
2014-06-14 15:31:19 -0400

    Add services to autoload path

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Mapper
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.autoload_paths += %W( #{File.join(config.root, "app", "services")} )
   end
 end
