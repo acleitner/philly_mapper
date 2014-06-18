2fb091fe610a369523e2f5aba5d8c750c1c52609
Aaron Leitner <aaron.leitner14@gmail.com>
2014-06-18 11:53:41 -0400

    Fix column name error that never got changed

--- app/models/geographic_entity.rb
+++ app/models/geographic_entity.rb
@@ -3,5 +3,5 @@ class GeographicEntity < ActiveRecord::Base
   has_many :map_elements
   has_many :maps, :through => :map_elements
 
-  set_rgeo_factory_for_column(:latlon, RGeo::Geographic.spherical_factory(:srid => 4326))
+  set_rgeo_factory_for_column(:geospatial_data, RGeo::Geographic.spherical_factory(:srid => 4326))
 end
