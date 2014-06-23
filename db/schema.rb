adb7a2f08032308124e1e7e1d63cb65231d349ea
Aaron Leitner <aaron.leitner14@gmail.com>
2014-06-23 15:07:51 -0400

    Rename app followup

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618154627) do
+ActiveRecord::Schema.define(version: 20140623165937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entity_sets", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "geographic_entities", force: true do |t|
     t.string   "name"
